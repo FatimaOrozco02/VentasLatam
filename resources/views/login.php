ff30943e05e7861608ccb28eae27b32eac281239
JeyS7 <jsalazar@difusion.com.mx>
2026-06-12 14:05:05 -0600

    Vista Login

--- resources/views/login.php
+++ resources/views/login.php
@@ -1,23 +1,61 @@
-<div class="w-100 d-flex justify-content-center mt-5">
-    <form id="loginSystem" method="post" class="w-50">
-        <div class="bg-bi-color-blue p-3 rounded d-flex flex-wrap gap-3">
-            <div class="w-100 d-flex justify-content-center">
-                <div class="logo-gdc">
-                    <img src="img/crm_gdc/logo_gdc.jpg" alt="Logo GDC">
-                </div>
+<section class="min-vh-100 section-login">
+   <div class="container py-5 px-5">
+      <div class="col-12 col-sm-12 col-md-12 pt-5 mb-5">
+
+         <div class="card mt-5 w-100 rounded-5 border-0">
+            <div class="row">
+               <div class="col-12 col-sm-12 col-md-7">
+                  <div class="bg_login">
+                     <div class="p-md-5"></div>
+                     <div class="p-5">
+                        <h1 class="title-login mt-5">Grupo Difusión Científica</h1>
+                        <h5 class="sub-title-login">Panel de control de ventas</h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-12 col-sm-12 col-md-5">
+                  <div class="mt-5 mb-5">
+                     <form id="formLogin" class="p-md-4 p-3">
+
+                        <div class="mt-5 mb-3">
+                           <h3 class="title-form">Bienvenido</h3>
+                        </div>
+                        <!-- usuario  -->
+                        <div class="input-group mb-3">
+                           <span class="input-group-text input-group-text-icon"><i class="fa-solid fa-user"></i></span>
+                           <input type="text" class="form-control border-left-none" id="loginName" name="name" placeholder="Nombre de usuarios">
+                        </div>
+                        <!-- contraseña  -->
+                        <div class="input-group mb-3">
+                           <span class="input-group-text input-group-text-icon"><i class="fa-solid fa-lock-open"></i></span>
+                           <input type="password" class="form-control border-left-none" placeholder="Contraseña" id="loginPassword" name="password">
+                           <span class="input-group-text input-group-eye"><i class="fa-regular fa-eye text-white"></i></span>
+                        </div>
+
+                        <div class="row">
+                           <div class="col-12 col-sm-12 col-md-12 mb-2">
+                              <div class="form-check">
+                                 <input class="form-check-input pointer" type="checkbox" value="" id="loginRemember" name="remember">
+                                 <label class="form-check-label" for="loginRemember">
+                                    Recordarme
+                                 </label>
+                              </div>
+                           </div>
+                           <div class="col-12 col-sm-12 col-md-12 mb-2">
+                              <p><a class="pointer">¿Olvidaste tu contraseña?</a></p>
+                           </div>
+                        </div>
+
+                        <div class="col-auto text-center mt-5">
+                           <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                        </div>
+
+                     </form>
+                  </div>
+               </div>
             </div>
-            <div class="group-floating w-100">
-                <input id="loginEmail" type="email" class="form-control f-input" placeholder="Correo electrónico">
-                <label for="loginEmail" class="f-label">Correo electrónico</label>
-            </div>
-            <div class="group-floating w-100">
-                <input id="loginPassword" type="password" class="form-control f-input" placeholder="Contraseña">
-                <label for="loginPassword" class="f-label">Contraseña</label>
-            </div>
-            <div class="w-100 d-flex justify-content-center align-items-center gap-3">
-                <button type="submit" formaction="login/simple" class="btn btn-dark-blue w-20">Ingresar</button>
-                <button id="loginOutlook" type="button" formaction="login/outlook" class="btn btn-primary w-20">Ingresar Outlook</button>
-            </div>
-        </div>
-    </form>
-</div>
+         </div>
+
+      </div>
+   </div>
+</section>
